53723ad7e1956fe0d0e8a7abaf4d998af242ac7d
Meenakshi <meenakshi@wisewaytec.com>
2024-08-09 18:13:09 +0530

    added about section

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/header/header.component';
 import { ProductsComponent } from './components/products/products.component';
+import { AboutComponent } from './components/about/about.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [HeaderComponent, ProductsComponent],
+  imports: [HeaderComponent, ProductsComponent, AboutComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })

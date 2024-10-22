fcdddc15846c242bb822fd11f082249097bdde13
Meenakshi <meenakshi@wisewaytec.com>
2024-10-22 15:02:30 +0530

    update css to show image of info page

--- src/app/components/home/components/company-info/company-info.component.ts
+++ src/app/components/home/components/company-info/company-info.component.ts
@@ -9,9 +9,29 @@ import { Component } from '@angular/core';
 })
 export class CompanyInfoComponent {
   company_info = [
-    { count: 45, title: 'Customers', image:'/company_info/customers.png' },
-    { count: 100, title: 'Orders', image:'/company_info/orders.png' },
-    { count: 2, title: 'Partners',image:'/company_info/partners.png' },
-    { count: 4, title: 'Investors', image:'/company_info/investors.png' },
+    {
+      count: 45,
+      title: 'Customers',
+      image: '/company_info/customers.png',
+      background: '#dc35457a',
+    },
+    {
+      count: 100,
+      title: 'Orders',
+      image: '/company_info/orders.png',
+      background: 'rgb(74 205 191 / 44%)',
+    },
+    {
+      count: 2,
+      title: 'Partners',
+      image: '/company_info/partners.png',
+      background: 'rgb(255 215 0 / 33%)',
+    },
+    {
+      count: 4,
+      title: 'Investors',
+      image: '/company_info/investors.png',
+      background: 'rgb(115 40 3 / 34%)',
+    },
   ];
 }
